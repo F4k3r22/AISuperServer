919b449cada667ac4dee4e87c9bf04ae272ca0d9
F4k3r22 <fredyriveraacevedo13@gmail.com>
2025-03-07 20:05:51 -0600

    Se experimenta con la Integración de FastAPI

--- AISuperServer/__init__.py
+++ AISuperServer/__init__.py
@@ -7,4 +7,3 @@ __version__ = '0.1.8'
 from .localmodel import AILocal
 from .create_server import create_inference_server as SuperServer
 from .inferenceapi import InferenceClient as SuperServerClient
-from .diff import TextToImagePipelineSD3
